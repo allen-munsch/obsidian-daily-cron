8e75aa34b966ce58fab5c21e720231ff36c14d91
allen-munsch <james.a.munsch@gmail.com>
2023-06-07 18:10:30 -0500

    Empty commit

--- daily-push.sh
+++ daily-push.sh
@@ -2,20 +2,20 @@
 
 source ~/.bashrc
 # Change to your repository directory
-REPO_DIR="$OBSIDIAN_CRON_REPO_DIR"
+export REPO_DIR="$OBSIDIAN_CRON_REPO_DIR"
 
 # Change to your remote repository URL
-REMOTE_URL="$OBSIDIAN_CRON_REMOTE_URL"
+export REMOTE_URL="$OBSIDIAN_CRON_REMOTE_URL"
 
 # Change to your Git user information
-GIT_USER_NAME="$OBSIDIAN_CRON_GIT_USER_NAME"
-GIT_USER_EMAIL="$OBSIDIAN_CRON_GIT_USER_EMAIL"
+export GIT_USER_NAME="$OBSIDIAN_CRON_GIT_USER_NAME"
+export GIT_USER_EMAIL="$OBSIDIAN_CRON_GIT_USER_EMAIL"
 
 # Move to the repository directory
 echo "REPO_DIR: $REPO_DIR"
 pushd "$REPO_DIR"
 echo "Currently looking at: $(pwd)"
-gst
+git status
 # Check if there are any changes
 if [[ -n $(git status -s) ]]; then
     # Add all changes
